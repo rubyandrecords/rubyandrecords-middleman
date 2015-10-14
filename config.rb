08e910ef8c0240736dddb3618e91af5587cf8c3d
jnf <jeremy.flores@gmail.com>
2015-10-14 09:48:11 -0700

    take a stab at config & deploy.

--- config.rb
+++ config.rb
@@ -56,17 +56,31 @@ set :images_dir, 'images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
+
+# after_build do |builder|
+#   src = "./CNAME"
+#   dst = File.join(config[:build_dir],"CNAME")
+#   builder.source_paths << File.dirname(__FILE__)
+#   builder.copy_file(src,dst)
+# end
+
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.remote = 'git@github.com:rubyandrecords/rubyandrecords.github.io.git'
+  deploy.branch = 'master'
+end
+
